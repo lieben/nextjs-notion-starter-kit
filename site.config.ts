392bf2a7e155d7c5db3cc1caaaea3b7feff57a4f
lieben <dielf_com@hotmail.com>
2024-07-21 03:54:26 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'AIM-CAFE-92ed8558174e4947ade179e70bce7e12',
+  rootNotionPageId: '92ed8558174e4947ade179e70bce7e12',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
